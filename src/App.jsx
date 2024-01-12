ca909f098dc6d60d931e124065da17b919f93d59
Md Parvej <mdparvejme24@gmail.com>
2024-01-12 21:07:05 +0600

    creaate square component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
-export default function Square() {
+
+function Square() {
+  return <button className="bg-white border border-gray-400 h-12 w-12 m-1">X</button>
+}
+
+export default function Board() {
   return (
     <>
-      <button className="bg-white border border-gray-400 h-12 w-12 m-1">X</button>
-      <button className="bg-white border border-gray-400 h-12 w-12 m-1">X</button>
+      <div>
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div>
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div>
+        <Square />
+        <Square />
+        <Square />
+      </div>
     </>
   )
 }
